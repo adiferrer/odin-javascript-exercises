c181edf0552f1af5185aa410e8c88eb1d8b72de6
adiferrer <adiferrer@gmail.com>
2022-05-05 17:49:20 +0800

    Add solution for exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const stringCharacters = str.split("");
+    let reversedString = "";
+    for (let i = stringCharacters.length - 1; i >=0; i--) {
+        reversedString += stringCharacters[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
