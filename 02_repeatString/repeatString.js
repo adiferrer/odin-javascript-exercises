0513bd5bf342caeaada730b23d8cf8ffd995e5e4
adiferrer <adiferrer@gmail.com>
2022-05-05 17:22:59 +0800

    Add solution to exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let finalString = "";
+    if (num >= 0) {
+        for(let i = 0; i < num; i++) {
+            // using the addition concatenation
+            // finalString += string;
+            
+            // using the concat method
+            finalString = finalString.concat(string);
+        }
+        return finalString;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
