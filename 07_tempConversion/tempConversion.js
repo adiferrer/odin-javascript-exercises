7df27f49bf043e01c4fbf96e468bc37d18342cec
adiferrer <adiferrer@gmail.com>
2022-05-06 12:16:54 +0800

    Complete Fundamentals Part 4 exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  if (fahrenheit == 32) {
+    return 0;
+  }
+  const celcius = (fahrenheit - 32) * (5/9);
+  return parseFloat(celcius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  if (celsius == 0) {
+    return 32;
+  }
+  const fahrenheit = celsius * (9/5) + 32;
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
