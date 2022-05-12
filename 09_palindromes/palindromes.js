cebb553dfcc0e349ce928569e886b947e6e3581a
adiferrer <adiferrer@gmail.com>
2022-05-12 15:00:46 +0800

    Solve palindromes code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase()
+             .split('')
+             .filter(c => c >= 'a' && c <= 'z')
+             .reverse()
+             .join(''); 
+    const reversed = str.toLowerCase()
+                        .split('')
+                        .filter(c => c >= 'a' && c <= 'z')
+                        .reverse()
+                        .join('');
+    return reversed == str;
 };
 
 // Do not edit below this line
