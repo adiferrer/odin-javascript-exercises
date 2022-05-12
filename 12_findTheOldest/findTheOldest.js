d11c175bbf2215547caa72bd56dae2e521f9be2d
adiferrer <adiferrer@gmail.com>
2022-05-12 18:33:30 +0800

    Finish fundamentals 5 exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((obj, item) => {
+        let prevAge = 0;
+        if ('yearOfDeath' in obj) prevAge = obj.yearOfDeath - obj.yearOfBirth;
+        else prevAge = (new Date().getFullYear()) - obj.yearOfBirth;
 
+        let newAge = 0;
+        if ('yearOfDeath' in item) newAge = item.yearOfDeath - item.yearOfBirth;
+        else newAge = (new Date().getFullYear()) - item.yearOfBirth;
+
+        if (prevAge < newAge) return item;
+        return obj;
+    }, people[0]);
 };
 
 // Do not edit below this line
