122bb29555d1cf95bf666c04002603eaeffeda5e
adiferrer <adiferrer@gmail.com>
2022-05-12 17:32:58 +0800

    Add fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
+    if (n === 1 || n === 2) return 1;
+    else if (n <= 0) return 'OOPS';
 
+    let firstTerm = 1;
+    let secondTerm = 1;
+    let fibo = 0;
+    for (let i = 3; i <= n; i++) {
+        fibo = firstTerm + secondTerm;
+        firstTerm = secondTerm;
+        secondTerm = fibo;
+    }
+    return fibo;
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
