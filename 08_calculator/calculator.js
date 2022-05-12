e2c751c1decef26d7100212ac72afa31cbc6c612
adiferrer <adiferrer@gmail.com>
2022-05-12 14:46:52 +0800

    Solve calculator methods

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const sum = array.reduce((total, number) => {
+    return total + number;
+  }, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(...numbers) {
+  const product = Array.from(...numbers)
+                       .reduce((total, number) => {
+                          return total * number;
+                        }, 1);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let power = 1;
+  for (let i = 1; i <= b; i++) power *= a;
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let factorial = 1;
+  if (n === 0) return factorial;
+  for (let i = 2; i <= n; i++) factorial *= i;
+  return factorial;
 };
 
 // Do not edit below this line
